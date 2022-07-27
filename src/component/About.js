5268886c8d4ac0e1544638350563ee362a50da57
rkddusen <k99endus@naver.com>
2022-07-27 20:56:42 +0900

    edit about page style

--- src/component/About.js
+++ src/component/About.js
@@ -3,16 +3,18 @@ import Nav from './Nav';
 import Footer from './Footer';
 import '../css/About.css';
 import profile from "../images/profile.png";
-import aboutme from "../images/aboutme.png";
 import career from "../images/career.jpg";
+import hobby from "../images/hobby.jpg";
 import skills from "../images/skills.png";
 function About(props) {
     return (
         <div className="about_section">
             <Nav />
-            <div className='about_screen about_screen_one'>
+            <div className='about_area'>
                 {/* <p className='about_profile_text'>Artistic Developer</p> */}
-                <p className='about_profile_text'>
+                <p>About me</p>
+                <div className='about_title'>
+                    <span>"</span>
                     <span className='text1'>Ar</span>
                     <span className='text2'>ti</span>
                     <span className='text3'>st</span>
@@ -22,11 +24,72 @@ function About(props) {
                     <span className='text6'>ve</span>
                     <span className='text7'>lo</span>
                     <span className='text8'>per</span>
-                </p>
-                <img src={profile} alt='profile' className='about_profile_img' />
-            </div>
-            <div className='about_screen about_screen_two'>
-                <div className='about_profile_explain about_aboutme'>
+                    <span>"</span>
+                </div>
+                <div className='about_contents'>
+                    <div className='profile'>
+                        <img src={profile} alt='profile' className='detail_img' />
+                        <div className='detail'>
+                            <p className='detail_title'>
+                                Profile
+                            </p>
+                            <p className='detail_text'>
+                                <b>이름</b> : 강두연<br />
+                                <b>성별</b> : 남자<br />
+                                <b>생년월일</b> : 199.06.30 (나이 : 24세)<br />
+                                <b>학력</b> : 건국대학교 재학 중<br />
+                                <b>주소</b> : 경기도 성남시 분당구<br />
+                                <b>좌우명</b> : ᶠᶸᶜᵏᵧₒᵤ 코딩
+                            </p>
+                        </div>
+                    </div>
+                    <div className='career'>
+                        <div className='detail'>
+                            <p className='detail_title'>Career</p>
+                            <p className='detail_text'>
+                                <b>2018.03</b> 건국대학교 소프트웨어학과 입학<br />
+                                <b>2019.03 - 2020.02</b> 건국대학교 아마추어 오케스트라 동아리 16기 부회장<br />
+                                <b>2019.07 - 2019.12</b> 2019년 sw활동 장학1<br />
+                                <b>2020.05 - 2021.11</b> 육군 복무<br />
+                                <b>2022.03 - xxxx.xx</b> 건국대학교 아마추어 오케스트라 동아리 홈페이지 제작<br />
+                            </p>
+                        </div>
+                        <img src={career} alt='profile' className='detail_img' />
+                    </div>
+                    
+                    <div className='skills'>
+                        <img src={skills} alt='profile' className='detail_img' />
+                        <div className='detail'>
+                            <p className='detail_title'>Skills</p>
+                            <p className='detail_text detail_right'>
+                                C언어 <progress value="50" max="100"></progress><br />
+                                C++ <progress value="10" max="100"></progress><br />
+                                Java <progress value="50" max="100"></progress><br />
+                                Python <progress value="50" max="100"></progress><br />
+                                Html, css <progress value="80" max="100"></progress><br />
+                                JavaScript <progress value="80" max="100"></progress><br />
+                                React <progress value="50" max="100"></progress><br />
+                                MySql <progress value="80" max="100"></progress><br />
+                                Nodejs <progress value="70" max="100"></progress><br />
+                            </p>
+                        </div>
+                        
+                    </div>
+                    <div className='hobby'>
+                        
+                        <div className='detail'>
+                            <p className='detail_title'>Hobby</p>
+                            <p className='detail_text'>
+                                먹고<br />
+                                눕고<br />
+                                자고<br />
+                                노는 거<br />
+                            </p>
+                        </div>
+                        <img src={hobby} alt='profile' className='detail_img' />
+                    </div>
+                </div>
+                {/* <div className='about_profile_explain about_aboutme'>
                     <div className='about_explain_img'><img src={aboutme} alt='img' /></div>
                     <div className='about_explain_detail'>
                         <strong>about me</strong>
@@ -46,7 +109,7 @@ function About(props) {
                         <strong>skills</strong>
                         <p>detail</p>
                     </div>
-                </div>
+                </div> */}
             </div>
             <Footer />
         </div>
